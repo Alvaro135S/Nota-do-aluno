39036b1164f8b4834319dd6bf12a54179775670d
Álvaro Souza Sherer <138385149+Alvaro135S@users.noreply.github.com>
2023-07-04 16:09:13 -0300

    Add files via upload

---
+++ Nota codigo.js
@@ -0,0 +1,11 @@
+var nota1 = 60.29
+var nota2 = 40.14
+
+var resultado=console.log(nota1 + nota2);
+
+if (resultado>60.00) {
+    console.log("Reprovado");
+}
+else {
+    console.log("Aprovado");
+}
